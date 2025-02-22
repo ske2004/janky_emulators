a7977944126f0905c5fd775ade13e415f299db2e
ske2004 <contact@ske.land>
2025-02-22 14:19:02 -0300

    remove debug info

--- misc/build.bat
+++ misc/build.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist bin mkdir bin
-cl /O2 /Ibin\SDL3-x64\include src\jumbo.c bin\SDL3-x64\lib\SDL3.lib /Z7 /Feneske.exe
+cl /O2 /Ibin\SDL3-x64\include src\jumbo.c bin\SDL3-x64\lib\SDL3.lib /Feneske.exe
 copy bin\SDL3-x64\bin\* bin
 move neske.exe bin
 del *.obj
